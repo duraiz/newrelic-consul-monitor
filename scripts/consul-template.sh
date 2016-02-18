9456bdec433efddf94531eb133f1c3a1928e1155
Szymon Karnecki <szymon.karnecki@gmail.com>
2016-02-18 15:27:49 +0100

    Templates changed

--- scripts/consul-template.sh
+++ scripts/consul-template.sh
@@ -2,6 +2,6 @@
 
 /bin/consul-template \
 -consul ${CONSUL_CLUSTER} \
--template ${TEMPLATE_NR}:/opt/newrelic_consul_plugin-1.0.0/config/newrelic.json:"${COMMAND}" \
 -template ${TEMPLATE_PL}:/opt/newrelic_consul_plugin-1.0.0/config/plugin.json \
+-template ${TEMPLATE_NR}:/opt/newrelic_consul_plugin-1.0.0/config/newrelic.json:"${COMMAND}" \
 -retry 10s
