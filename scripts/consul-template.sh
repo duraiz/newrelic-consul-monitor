7f1d35fa125fbc671be6dbe7b99ab80dee718f0b
Szymon Karnecki <szymon.karnecki@gmail.com>
2016-02-18 14:54:33 +0100

    Update consul-template retry

--- scripts/consul-template.sh
+++ scripts/consul-template.sh
@@ -3,4 +3,5 @@
 /bin/consul-template \
 -consul ${CONSUL_CLUSTER} \
 -template ${TEMPLATE_NR}:/opt/newrelic_consul_plugin-1.0.0/config/newrelic.json:"${COMMAND}" \
--template ${TEMPLATE_PL}:/opt/newrelic_consul_plugin-1.0.0/config/plugin.json:"${COMMAND}"
+-template ${TEMPLATE_PL}:/opt/newrelic_consul_plugin-1.0.0/config/plugin.json:"${COMMAND}" \
+-retry 10s
