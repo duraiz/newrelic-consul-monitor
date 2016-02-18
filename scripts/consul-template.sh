026f60036fb367eecdaaf77facb16844c8d45a10
Szymon Karnecki <szymon.karnecki@gmail.com>
2016-02-18 15:20:27 +0100

    Consul template triggering

--- scripts/consul-template.sh
+++ scripts/consul-template.sh
@@ -3,5 +3,5 @@
 /bin/consul-template \
 -consul ${CONSUL_CLUSTER} \
 -template ${TEMPLATE_NR}:/opt/newrelic_consul_plugin-1.0.0/config/newrelic.json:"${COMMAND}" \
--template ${TEMPLATE_PL}:/opt/newrelic_consul_plugin-1.0.0/config/plugin.json:"${COMMAND}" \
+-template ${TEMPLATE_PL}:/opt/newrelic_consul_plugin-1.0.0/config/plugin.json \
 -retry 10s
